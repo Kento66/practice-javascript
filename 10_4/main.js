c994eda031f1fec469f9a2c6394f20dd4149ec0c
Kento66 <ransu.nana06@gmail.com>
2024-10-04 06:02:01 +0000

    10月4日　写経

---
+++ 10_4/main.js
@@ -0,0 +1,19 @@
+//クリックされたら動作する
+
+function errorInput(){
+    const month = document.getElementById("month").value;
+    const day = document.getElementById("day").value
+    if(month<1 || month>12){
+        alert("適切な値ではありません");
+        return
+    }
+    console.log("Month:",month,"Day:",day)
+}
+//docment.getElementById("month")はタグを確認するだけ
+//valueはそのタグの値を示す
+
+/*
+javaScriptのプログラムとHTMLファイルはどこで対応しているか
+タグについているIDで紐づく
+タグの中にある負荷情報のことを属性という
+*/
