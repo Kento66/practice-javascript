fe992ff83309e8a4f2425d433792d3f2edba7aa1
Kento66 <ransu.nana06@gmail.com>
2025-01-31 06:18:59 +0000

    セレクターの追加

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+const gameSelector = document.getElementById('game-select')// game-select gameSelector
+const gameContainer = document.getElementById('game-container')
+gameSelector.addEventListener('change',function(){
+ gameContainer.innerHTML='';
+ switch(gameSelector.value) {
+    case 'number-guess':
+    startNumberGuessGame();
+      break;
+   case'clicker':
+      ClickCounterGame();
+     break;
+ }
+ });
+
 function ClickCounterGame(){
     let count = 0;
     const gameContainer = document.getElementById("game-container");
@@ -37,7 +51,7 @@ function ClickCounterGame(){
     gameContainer.appendChild(button3);
     gameContainer.appendChild(counter);
 }
-ClickCounterGame();
+
 
 function startNumberGuessGame(){
     const gameContainer = document.getElementById("game-container");
@@ -49,6 +63,32 @@ function startNumberGuessGame(){
     input.placeholder = '好きな数字を入力してください(1-100)';
     gameContainer.appendChild(message);
     gameContainer.appendChild(input);
-}
 
-startNumberGuessGame();
+    let button = document.createElement("button")
+    button.textContent = "確認"
+
+    button.addEventListener("click",function(){
+
+        const val = parseInt(input.value);
+
+        if (randomNumber == val){
+            message.textContent="正解"
+        }
+        else if(randomNumber<val){
+            message.textContent = "大きい"
+        }
+        else{
+            message.textContent="小さい"
+        }
+        count++;
+        countDisplay.textContent=`試行回数: ${count}`;
+    })
+
+    gameContainer.appendChild(button);
+
+    let count=0;
+
+    const countDisplay=document.createElement("countDisplay");
+    countDisplay.textContent=`試行回数: ${count}`;
+    gameContainer.appendChild(countDisplay);
+}
